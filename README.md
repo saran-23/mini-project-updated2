02d3bb39b145ff2f0221127f7082e299a838da6a
saran-23 <romansaran446@gmail.com>
2021-11-11 09:01:32 -0800

    works fine :)

---
+++ README.md
@@ -0,0 +1,28 @@
+# Object Detection
+Object Detection on the browser using Tensorflow.js
+# Dependencies
+
+* [TensorFlow.js](https://www.tensorflow.org/js)
+* [Keras](https://keras.io/)
+* [npm](https://www.npmjs.com/)
+* [React.js](https://reactjs.org/)
+
+# Installation
+
+### Prerequisites
+
+Install Node.js 
+
+### Instructions
+
+Codel", which contains the model.json, label.json and the shard files.
+
+Run the project
+```bash
+npm start
+```
+
+Open a web browser and go to
+```bash
+http://localhost:3000
+```
---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React, { useState, useCallback } from 'react';
+import * as tf from '@tensorflow/tfjs';
+import { ModelContext } from './components/context/model-context';
+import Selector from './select';
+import LoadingSpinner from './components/utils/LoadingSpinner';
+
+const MODEL_URL = process.env.PUBLIC_URL + '/model/';
+const LABELS_URL = MODEL_URL + 'labels.json';
+const MODEL_JSON = MODEL_URL + 'model.json';
+
+const App = () => {
+  const [model, setModel] = useState(null);
+  const [labels, setLabels] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [selected, setSelected] = useState('');
+
+  const fetchModel = useCallback((model) => {
+      setModel(model);
+  }, []);
+
+  const fetchLabels = useCallback((labels) => {
+      setLabels(labels);
+  }, []);
+
+  const selectMode = useCallback((selected) => {
+      setSelected(selected)
+  }, [])
+
+  const loadModel = async () => {
+    setLoading(true);
+    const model = await tf.loadGraphModel(MODEL_JSON);
+    fetchModel(model);
+    const response = await fetch(LABELS_URL);
+    let labels = await response.json();
+    fetchLabels(labels)
+    setLoading(false);
+  }
+
+  return (
+      <ModelContext.Provider 
+        value={{
+          model: model, 
+          fetchModel: fetchModel, 
+          labels: labels, 
+          fetchLabels: fetchLabels,
+          selected: selected,
+          selectMode: selectMode
+        }}>
+
+          <div>
+            {model ? (
+              <div>
+                <Selector />
+              </div>
+            ) : (
+              <div className="center-div load-div">
+                  {loading ? (
+                    <div style={{ textAlign: 'center' }}>
+                      <LoadingSpinner />
+                      <p style={{
+                        color: 'blue',
+                        fontWeight: '500',
+                      }}>Loading Model. Please wait a few seconds...</p>
+                    </div>
+                  ) : (
+                    <button className="css-btn" style={{ width: '60%' }} onClick={loadModel}>
+                      <p style={{ fontSize: '16px', fontWeight: '500' }}>Load Model</p>
+                    </button>
+                  )}
+              </div>
+            )}
+          </div>
+      </ModelContext.Provider>
+  );
+}
+
+export default App;
+
+
