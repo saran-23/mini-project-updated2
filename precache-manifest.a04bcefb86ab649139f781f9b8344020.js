c61cc76da770b5fc71183c0153cdb199d8ef0eb1
saran-23 <romansaran446@gmail.com>
2021-11-11 09:28:44 -0800

    Updates

---
+++ precache-manifest.a04bcefb86ab649139f781f9b8344020.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb614a254ba4996d47089a3647024d9a",
+    "url": "/mini-project-updated2/index.html"
+  },
+  {
+    "revision": "f72f61afd84c0a6483ac",
+    "url": "/mini-project-updated2/static/css/main.3bf0efc9.chunk.css"
+  },
+  {
+    "revision": "b0d15daee8961bcc2a1b",
+    "url": "/mini-project-updated2/static/js/2.a2d5ec12.chunk.js"
+  },
+  {
+    "revision": "203136b256d73b86d9b5f7bd77fb7e0d",
+    "url": "/mini-project-updated2/static/js/2.a2d5ec12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f72f61afd84c0a6483ac",
+    "url": "/mini-project-updated2/static/js/main.68a8db72.chunk.js"
+  },
+  {
+    "revision": "a279800cc08d4b13cbe8",
+    "url": "/mini-project-updated2/static/js/runtime-main.ed18a686.js"
+  }
+]);
